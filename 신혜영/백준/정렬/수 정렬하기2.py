9994dda29cad7c01c4b51ba10f9ba23afa22f179
HyeYoung Shin <nanda5050@naver.com>
2021-10-10 23:28:00 +0900

    Create 수 정렬하기2.py

---
+++ 신혜영/백준/정렬/수 정렬하기2.py
@@ -0,0 +1,22 @@
+num = int(input())
+arr = []
+for i in range(num):
+    arr.append(int(input()))
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    lesser_arr, equal_arr, greater_arr = [], [], []
+    for num in arr:
+        if num < pivot:
+            lesser_arr.append(num)
+        elif num > pivot:
+            greater_arr.append(num)
+        else:
+            equal_arr.append(num)
+    return quick_sort(lesser_arr) + equal_arr + quick_sort(greater_arr)
+
+arr = quick_sort(arr)
+for num in range:
+    print(num)
