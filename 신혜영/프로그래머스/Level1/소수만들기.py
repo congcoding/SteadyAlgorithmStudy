243c5a3a7f698141808ed8d990a6d79cf0d158eb
HyeYoung Shin <nanda5050@naver.com>
2021-11-07 00:55:09 +0900

    Create 소수만들기.py

---
+++ 신혜영/프로그래머스/Level1/소수만들기.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+def isPrime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+def solution(nums):
+    answer = 0
+    
+    com = list(combinations(nums, 3))
+    for i in com:
+        if isPrime(sum(i)):
+            answer += 1
+
+    return answer
