461eaf2398ced1cb8d492cc7b850b4e599bda730
JeongWon Been <congcoding@gmail.com>
2021-11-07 21:19:50 +0900

    Create 중복조합.java

---
+++ 빈정원/📔Notes/중복조합.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	static int N, M;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		
+		combination(0, 0, new int[M]);
+		System.out.println(sb.toString());
+	}
+
+	private static void combination(int start, int cnt, int[] nums) {
+		if (cnt == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(arr[nums[i]] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = start; i < N; i++) {
+			nums[cnt] = i;
+			combination(i, cnt + 1, nums);
+		}
+	}
+
+
+}
